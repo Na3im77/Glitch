d811711121b597f2da6a8068bf9ea348505c7eff
Na3im77 <152448880+Na3im77@users.noreply.github.com>
2023-11-30 00:56:22 +0100

    commit

---
+++ admin/src/util/Config.js
@@ -0,0 +1,3 @@
+exports.baseURL = "api.na3imdev-project-demo.online";
+
+exports.key = "No1IxsMPt}R|d|r!O^M>FRHOJv^~#n";
---
+++ backend/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  //server database
+ MONGODB_USERNAME: "naimcrown4041mc",
+ MONGODB_PASSWORD: "r8Os922gd2rdZSxr",
+ MONGODB_DB_NAME: "naimcrown40416383",
+
+ //port
+ PORT: Process.env.PORT || 5000,
+
+ //secret key for API
+ SECRET_KEY: "No1IxsMPt}R|d|r!O^M>FRHOJv^~#n",
+
+ //gmail credentials for send email
+ EMAIL: "chatcrown10@gmail.com",
+ PASSWORD: "hqtv bvub xbiu efpv",
+
+ //secret key for jwt
+ JWT_SECRET: "t?{j1-&]4]UENn<Ui+bfn^IM#3Vbhr",
+
+ //baseURL
+  baseURL: "api.na3imdev-project-demo.online",
+
+ //server key
+ SERVER_KEY : "AAAAMV4ERMw:APA91bExU-UnfzplpCgtu-pQjWfdsK5MH1L_Eoq0KDB3rI4P_YPmPjtE4rkSq6Iew9fkqaKDkyRqqQRDQzUWjXmLJnm45_XkLbOOi7HT-wiadQdWcH-_5iqgU5yb9AaTUNp6ktNJLuGE"
+
+};
